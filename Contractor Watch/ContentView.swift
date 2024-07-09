42a289cbc016531a939dbd66b63ae9adfe8ac190
Molanter <e.yarmolatiy@gmail.com>
2024-07-09 10:49:55 -0500

    Initial Commit

---
+++ Contractor Watch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Contractor Watch
+//
+//  Created by Edgars Yarmolatiy on 7/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Contractor Watch/Contractor_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contractor_WatchApp.swift
+//  Contractor Watch
+//
+//  Created by Edgars Yarmolatiy on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Contractor_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
